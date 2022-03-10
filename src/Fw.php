f1ec6ff2edfe7e939caa677db80509a5a935bcd2
Eko Kurniawan <setiawanegho@gmail.com>
2022-03-10 15:25:53 +0700

    Fw aliasing as static

--- src/Fw.php
+++ src/Fw.php
@@ -85,7 +85,7 @@ class Fw
 
     public function __construct(private string|null $env, private Di $di, private Box $box)
     {
-        $this->di->inject($this, array('alias' => 'fw', 'name' => self::class));
+        $this->di->inject($this, array('alias' => 'fw', 'name' => static::class));
         $this->di->inject($this->box, array('alias' => 'box', 'name' => Box::class));
 
         $this->initialize();
